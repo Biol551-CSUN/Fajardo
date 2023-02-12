b85560a5c52f252e0d8e5165564cd4ea4f809052
Cindy Fajardo <cindy.fajardo.625@my.csun.edu>
2023-02-11 21:40:12 -0800

    Updated homeowork

--- Week_03/Scripts/2023_02_07_homework_ggplot.R
+++ Week_03/Scripts/2023_02_07_homework_ggplot.R
@@ -11,19 +11,13 @@ library(here)
 library(devtools)
 library(beyonce)
 library(ggthemes)
+library(magrittr)
 
 View(penguins)
 #will work on sex and body mass per species
 
-penguins %>% 
-  na.omit() %>% ##omits na's in data
-ggplot(penguins %>% na.omit,
-      mapping = aes(x = sex,
-                    y = body_mass_g,
-                    group = species,
-                    color = species))
 
-penguins_summary <- penguins%>% ##summarizes data by sex and species
+penguins_summary <- penguins %>%
   group_by(sex, species) %>% 
   summarize(mean=mean(body_mass_g),
             sum=sum(body_mass_g))
@@ -34,11 +28,31 @@ ggplot(penguins_summary %>% na.omit,
        mapping = aes(x = sex,
                      y = mean,
                      group = species,
-                     color = species,
                      fill = species)) +
   geom_col(position = "dodge2") +
   labs(x = "Sex of Penguins", 
        y = "Body Mass of Penguins (g)",
-       title="Body Mass of Penguins by Sex of Three Different Species") +
+       title="Body Mass of Penguins by Sex of Three Different Species",
+       color = "Species") +
     scale_fill_manual(values = beyonce_palette(18)) +
-  scale_color_manual(values = beyonce_palette(18))
+  scale_color_manual(values = beyonce_palette(18)) +
+  theme_classic()+
+  theme(plot.title = element_text(size = 15,
+                                  color = "purple"))
+ggsave(here("Week_03","Output","2023_02_14_Homework.png"))
+
+
+#####
+#Hanna's plot#
+ggplot(penguins %>% na.omit(),
+       mapping=aes(x=body_mass_g,
+                   group=species,
+                   fill=species)) +
+  geom_density() +
+  facet_grid(species~sex) +
+  theme_classic() +
+  labs(x="Body mass of Penguins (g)",
+       y="Density") +
+  scale_fill_manual(values= beyonce_palette(18)) + ##never using anything else (jk), Beyonce is a queen with these blues +
+  theme(axis.text.x=element_text(size=13),
+        axis.text.y=element_text(size=13))
